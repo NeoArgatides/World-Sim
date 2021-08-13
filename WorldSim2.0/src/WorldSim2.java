6bae1f8c8acbf2c58f67ab65e330bd87d192f68f
Darin <Darin@DESKTOP-JEJAQ40>
2021-08-12 23:07:06 -0700

    drake ???

--- WorldSim2.0/src/WorldSim2.java
+++ WorldSim2.0/src/WorldSim2.java
@@ -12,3 +12,18 @@ public class WorldSim2
 		
     }
 }
+
+
+class civilization {
+
+		String name = "";		
+		int population = 0;
+		int stage = 1; //demographic transition model
+		int wealth = 0; //gross domestic product (economy) (not evaluated until dtm stage 2)
+
+		
+		int food_supply = 0; //1 food unit feeds 1,000 people for a month
+		int health = 0; // it takes 150 farmers to produce a food unit in a month
+		int technology = 0;
+		
+}
