e671091ce64bcfd60c76b06aa3ca37c5ff2617ac
Darin <Darin@DESKTOP-JEJAQ40>
2021-08-20 22:25:36 -0700

    d             r               a                k                 e

--- WorldSim2.0/src/WorldSim2.java
+++ WorldSim2.0/src/WorldSim2.java
@@ -20,7 +20,7 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeries; 
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
